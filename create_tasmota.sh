09248da2472d2b020dac0c0d0851472ee0f42b27
smuuze <sebastia_n@web.de>
2021-05-15 19:36:47 +0200

    ADD: TASMOTA - new rules/items to create common reports

--- create_tasmota.sh
+++ create_tasmota.sh
@@ -6,6 +6,10 @@
 #
 #
 
+# Count Files that are already available
+# find /etc/openhab2/items/ -maxdepth 1 -type f | grep TASMOTA.items | wc -l
+client_count=$(find /etc/openhab2/items/ -maxdepth 1 -type f | grep TASMOTA.items | wc -l)
+new_client_count=$(expr $client_count + 1)
 
 cp oh2/items/myflat_LOCATION_TASMOTA.items myflat_$1_$2_TASMOTA.items
 cp oh2/rules/myflat_LOCATION_TASMOTA.rules myflat_$1_$2_TASMOTA.rules
@@ -13,26 +17,38 @@ cp oh2/rules/myflat_LOCATION_TASMOTA.rules myflat_$1_$2_TASMOTA.rules
 #echo "Replacing incorrect file endings"
 #sed -i -e 's/\r$//g' myflat_$1_$2.items
 
-echo "Set Location: $1"
+# Replace Location
 
+echo "Set Location: $1"
 sed -i "s/LOCATION_NAME/$1/g" myflat_$1_$2_TASMOTA.items
 sed -i "s/LOCATION_NAME/$1/g" myflat_$1_$2_TASMOTA.rules
+
+# Replace Client-Name
 
 echo "Set TASMOTA: $2"
 sed -i "s/CLIENT_NAME/$2/g" myflat_$1_$2_TASMOTA.items
 sed -i "s/CLIENT_NAME/$2/g" myflat_$1_$2_TASMOTA.rules
 
-echo "Moving File: myflat_$1_$2_TASMOTA.items"
+# Set __TIMER_DELAY__
 
-mv /etc/openhab2/items/myflat_$1_$2_TASMOTA.item /etc/openhab2/items/myflat_$1_$2_TASMOTA.old_items 2> /dev/null
+echo "Moving File: myflat_$1_$2_TASMOTA.items"
+sed -i "s/__TIMER_DELAY__/$new_client_count/g" myflat_$1_$2_TASMOTA.rules
+
+mv /etc/openhab2/items/myflat_$1_$2_TASMOTA.item /etc/openhab2/items/myflat_$1_$2_TASMOTA.old_items > /dev/null
 
 mv myflat_$1_$2_TASMOTA.items /etc/openhab2/items/myflat_$1_$2_TASMOTA.items
 chmod ug=+rwx /etc/openhab2/items/myflat_$1_$2_TASMOTA.items
 chown openhab:openhab /etc/openhab2/items/myflat_$1_$2_TASMOTA.items
 
+echo "Moving File: myflat_common_TASMOTA.items"
+
+cp oh2/items/myflat_common_TASMOTA.items /etc/openhab2/items/myflat_common_TASMOTA.items
+chmod ug=+rwx /etc/openhab2/items/myflat_common_TASMOTA.items
+chown openhab:openhab /etc/openhab2/items/myflat_common_TASMOTA.items
+
 echo "Moving File: myflat_$1_$2_TASMOTA.rules"
 
-mv /etc/openhab2/items/myflat_$1_$2_TASMOTA.rules /etc/openhab2/rules/myflat_$1_$2_TASMOTA.old_rules 2> /dev/null
+mv /etc/openhab2/items/myflat_$1_$2_TASMOTA.rules /etc/openhab2/rules/myflat_$1_$2_TASMOTA.old_rules > /dev/null
 
 mv myflat_$1_$2_TASMOTA.rules /etc/openhab2/rules/myflat_$1_$2_TASMOTA.rules
 chmod ug=+rwx /etc/openhab2/rules/myflat_$1_$2_TASMOTA.rules
