cd31cddc81b1996aa9e5e03e9f95d339c2d7290d
smuuze <sebastia_n@web.de>
2021-05-16 14:05:05 +0200

    FIX: Tasmota rules+items+create_script

--- create_tasmota.sh
+++ create_tasmota.sh
@@ -6,13 +6,43 @@
 #
 #
 
+# First: move old item to ignore values in it
+old_rule_file=/etc/openhab2/rules/myflat_$1_$2_TASMOTA.rules
+if test -f "$old_rule_file"; then
+	printf "Remofing old file: %s\n" $old_rule_file
+	mv $old_rule_file /etc/openhab2/rules/myflat_$1_$2_TASMOTA.old_rules
+fi
+
+old_item_file=/etc/openhab2/items/myflat_$1_$2_TASMOTA.items
+if test -f "$old_item_file"; then
+	printf "Remofing old file: %s\n" $old_item_file
+	mv $old_item_file /etc/openhab2/items/myflat_$1_$2_TASMOTA.old_items
+fi
+
 # Count Files that are already available
 # find /etc/openhab2/items/ -maxdepth 1 -type f | grep TASMOTA.items | wc -l
-client_count=$(find /etc/openhab2/items/ -maxdepth 1 -type f | grep TASMOTA.items | wc -l)
-new_client_count=$(expr $client_count + 1)
+#client_count=$(find /etc/openhab2/items/ -maxdepth 1 -type f | grep TASMOTA.items | wc -l)
+#new_client_count=$(expr $client_count + 1)
+max_value=0
 
-echo "Available clients: $client_count"
-echo "New client number: $new_client_count"
+for f in /etc/openhab2/rules/*_TASMOTA.rules; do
+
+	line=$(grep -i CLIENT_NUMBER $f)
+	value=${line//[!0-9]/}
+	value=$(expr $value + 0)
+
+	#printf "File: %s\n" $f
+	#printf "Line: %s\n" $line
+	#printf "Value: %d - Max: %d\n" $value $max_value
+
+	if [ "$value" -gt "$max_value" ]; then
+		max_value=$value
+		#printf "%s %d\n" "New maximum:" $max_value
+	fi
+done
+
+new_client_count=$(expr $max_value + 1)
+printf "New CLIENT_NUMBER: %d\n" $new_client_count
 
 cp oh2/items/myflat_LOCATION_TASMOTA.items myflat_$1_$2_TASMOTA.items
 cp oh2/rules/myflat_LOCATION_TASMOTA.rules myflat_$1_$2_TASMOTA.rules
@@ -40,8 +70,6 @@ echo "Moving File: myflat_$1_$2_TASMOTA.items"
 sed -i "s/__CLIENT_NUMBER__/$new_client_count/g" myflat_$1_$2_TASMOTA.rules
 sed -i "s/__CLIENT_NUMBER__/$new_client_count/g" myflat_$1_$2_TASMOTA.items
 
-mv /etc/openhab2/items/myflat_$1_$2_TASMOTA.item /etc/openhab2/items/myflat_$1_$2_TASMOTA.old_items > /dev/null
-
 mv myflat_$1_$2_TASMOTA.items /etc/openhab2/items/myflat_$1_$2_TASMOTA.items
 chmod ug=+rwx /etc/openhab2/items/myflat_$1_$2_TASMOTA.items
 chown openhab:openhab /etc/openhab2/items/myflat_$1_$2_TASMOTA.items
@@ -52,9 +80,13 @@ cp oh2/items/myflat_common_TASMOTA.items /etc/openhab2/items/myflat_common_TASMO
 chmod ug=+rwx /etc/openhab2/items/myflat_common_TASMOTA.items
 chown openhab:openhab /etc/openhab2/items/myflat_common_TASMOTA.items
 
-echo "Moving File: myflat_$1_$2_TASMOTA.rules"
+echo "Moving File: myflat_common_TASMOTA.rules"
 
-mv /etc/openhab2/items/myflat_$1_$2_TASMOTA.rules /etc/openhab2/rules/myflat_$1_$2_TASMOTA.old_rules > /dev/null
+cp oh2/rules/myflat_common_TASMOTA.rules /etc/openhab2/rules/myflat_common_TASMOTA.rules
+chmod ug=+rwx /etc/openhab2/rules/myflat_common_TASMOTA.rules
+chown openhab:openhab /etc/openhab2/rules/myflat_common_TASMOTA.rules
+
+echo "Moving File: myflat_$1_$2_TASMOTA.rules"
 
 mv myflat_$1_$2_TASMOTA.rules /etc/openhab2/rules/myflat_$1_$2_TASMOTA.rules
 chmod ug=+rwx /etc/openhab2/rules/myflat_$1_$2_TASMOTA.rules
